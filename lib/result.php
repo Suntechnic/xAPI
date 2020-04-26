9b2629fa25852df46efee886e1883b637e0c714a
Suntechnic <madzhugin@yandex.ru>
2020-04-26 23:48:59 +0300

    перегрузка методов addError и addErrors \X\Result

--- lib/result.php
+++ lib/result.php
@@ -22,6 +22,37 @@ class Result extends \Bitrix\Main\Result
 	public function getId($entity=false)
 	{
 		if ($entity) $this->ids[$entity] = $id;
+		
 		return $this->id;
+	}
+	
+	
+	/**
+	 * Adds the error.
+	 *
+	 * @param Error $error
+	 * @return $this
+	 */
+	public function addError($error)
+	{
+		if (!is_a($error,'\Bitrix\Main\Error')) $error = new \Bitrix\Main\Error($error);
+		return parent::addError($error);
+	}
+
+	/**
+	 * Adds array of Error objects
+	 *
+	 * @param Error[] $errors
+	 * @return $this
+	 */
+	public function addErrors(array $errors)
+	{
+		
+		$errors = array_map(function ($e) {
+			if (!is_a($e,'\Bitrix\Main\Error')) $e = new \Bitrix\Main\Error($e);
+			return $e;
+		}, $errors);
+		
+		return parent::addErrors($errors);
 	}
 }
