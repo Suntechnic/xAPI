afae476a59884c667a27089ff0e7bee587c13339
madzhugin <madzhugin@yandex.ru>
2020-12-17 13:57:51 +0300

    add stringstorage exemple 2

---
+++ x.exemple/model/stringstorage.php
@@ -0,0 +1,21 @@
+<?
+// \Model\Stringstorage::getInstance()->getStringVal(кодСтроки);
+/*
+Создать hl блок
+Stringstorage
+b_hlbd_stringstorage
+
+Список полей:
+UF_XML_ID	Строка
+UF_STRING	Строка
+UF_NAME	Строка
+
+
+*/
+namespace Model {
+    class Stringstorage extends \X\Abstraction\Protomodel\Stringstorage {
+        
+        const IDHLB = 1;
+        
+    }
+}
