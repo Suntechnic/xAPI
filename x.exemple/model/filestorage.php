471349aeeaf20274638880d9539e03d27a5d7037
madzhugin <madzhugin@yandex.ru>
2020-12-27 14:45:57 +0300

    + примеры

---
+++ x.exemple/model/filestorage.php
@@ -0,0 +1,21 @@
+<?
+// \Model\Filestorage::getInstance()->getFile(кодСтроки);
+/*
+Создать hl блок
+Filestorage
+b_hlbd_filestorage
+
+Список полей:
+UF_NAME	Строка
+UF_FILE	Файл
+UF_URL	Строка
+UF_XML_ID	Строка
+*/
+
+namespace Model {
+    class Filestorage extends \X\Abstraction\Protomodel\Filestorage {
+        
+        const IDHLB = 6;
+        
+    }
+}
