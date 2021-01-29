c9691fb2ea012f3ea3a3a5dedfd7c38de2b8f9aa
madzhugin <madzhugin@yandex.ru>
2021-01-29 22:37:12 +0300

    доработки по fs

---
+++ x.exemple/model/filestorage.php
@@ -0,0 +1,23 @@
+<?
+// \Model\Filestorage::getInstance()->getFileSrc();
+/*
+Создать hl блок
+Filestorage
+b_hlbd_filestorage
+
+Список полей:
+
+UF_NAME	Строка
+UF_FILE	Файл
+UF_URL	Строка
+UF_XML_ID	Строка
+
+
+*/
+namespace Model {
+    class Filestorage extends \X\Abstraction\Protomodel\Filestorage {
+        
+        const IDHLB = 2;
+        
+    }
+}
